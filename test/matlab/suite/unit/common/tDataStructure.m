8c989d653f10fe6fb1d070b253633c7653e12fb7
Andrea Picciau <andrea@picciau.net>
2019-11-22 11:02:35 +0000

    Strengthen the coverage of amsla.common.DataStructure in the unit tests.
    * This change exposed a bug. Test is not expected to pass at this stage.

--- test/matlab/suite/unit/common/tDataStructure.m
+++ test/matlab/suite/unit/common/tDataStructure.m
@@ -16,9 +16,9 @@ classdef tDataStructure < amsla.test.tools.AmslaTest
     % limitations under the License.
     
     
-    %% TEST METHODS
-    
     methods (Test)
+        
+        %% "Graph API" for nodes and edges
         
         function checkChildrenOfNodeVector(testCase)
             % Check the method "childrenOfNode" with a vector input.
@@ -115,6 +115,42 @@ classdef tDataStructure < amsla.test.tools.AmslaTest
                 loopEdgesOfNode(aGraph, nodes), ...
                 expectedOutput);
         end
+    end
+    
+    properties(TestParameter)
+        
+        EdgeNodeAssignment = struct( ...
+            'Scalar', struct( ...
+            'InputEdge', { 1 }, ...
+            'ExitNode',  { 1 }), ...
+            ...
+            'Vector', struct( ...
+            'InputEdge', { [1, 3, 5] }, ...
+            'ExitNode',  { [1, 3, 3] }));
+        
+    end
+    
+    methods(Test)
+        
+        function exitNodesOfEdgesMatchExpected(testCase, EdgeNodeAssignment)
+            % Check that, given some input edge IDs, the exit nodes are
+            % identified correctly,
+            
+            aGraph = amsla.test.tools.getSimpleLowerTriangularMatrix();
+            
+            inputEdge = EdgeNodeAssignment.InputEdge;
+            expectedExitNode = EdgeNodeAssignment.ExitNode;
+            
+            actualExitNode = aGraph.exitingNodeOfEdge(inputEdge);
+            
+            testCase.verifyEqual(actualExitNode, expectedExitNode, ...
+                "Output exit nodes are not what was expected.");
+        end
+        
+    end
+    %% Component-oriented API
+    
+    methods(Test)
         
         function checkListOfComponentsEmpty(testCase)
             % Check method "listOfComponents" when components where not
@@ -185,6 +221,73 @@ classdef tDataStructure < amsla.test.tools.AmslaTest
                 expectedOutput);
         end
         
+        function checkComponentOfNodeVector(testCase)
+            % Check method "componentOfNode" after components have been
+            % computed, when inputs are a vector.
+            
+            % Create a graph
+            [aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
+            
+            aGraph.computeComponents();
+            
+            inputs = 1:10;
+            expectedOutput = [ones(1,8), 2*ones(1,2)];
+            
+            testCase.verifyEqual( ...
+                componentOfNode(aGraph, inputs), ...
+                expectedOutput);
+        end
+        
+        %% Sub-graph API
+        
+        function listOfSubGraphsShouldBeEmptyWhenNotPartitioned(testCase)
+            % The method "listOfSubGraph" should be empty if the graph
+            % hasn't been partitioned.
+            
+            % Create a graph
+            [aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
+            
+            subGraphIds = aGraph.listOfSubgraphs();
+            testCase.verifyEmpty(subGraphIds, ...
+                "The list of sub-graphs should be empty when the graph hasn't been partitioned.");
+        end
+        
+        function listOfSubGraphShouldNotBeEmptyIfAtLeastOne(testCase)
+            % The method "listOfSubGraph" should not return an empty array if
+            % at least one node has been assigned to a sub-graph.
+            
+            % Create a graph
+            [aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
+            
+            aGraph.setSubGraphOfNode(1, 1);
+            subGraphIds = aGraph.listOfSubgraphs();
+            testCase.verifyNotEmpty(subGraphIds, ...
+                "The list of sub-graphs should not be empty when at least one node has been assigned.");
+        end
+        
+        function listOfSubGraphShouldMatchOnFullPartitioning(testCase)
+            % The method "listOfSubGraph" should return the IDS of all the
+            % sub-graphs when the partitioning is complete.
+            
+            aGraph = iFullAssignedSimpleGraph();
+            
+            subGraphIds = aGraph.listOfSubgraphs();
+            testCase.verifyEqual(subGraphIds, 1:5, ...
+                "The list of sub-graphs is incorrect.");
+        end
+        
+        function listOfSubGraphShouldBeEmptyAfterReset(testCase)
+            % The method "listOfSubGraph" should return an empty array after
+            % the method "resetSubGraphs".
+            
+            aGraph = iFullAssignedSimpleGraph();
+            aGraph.resetSubGraphs();
+            
+            subGraphIds = aGraph.listOfSubgraphs();
+            testCase.verifyEmpty(subGraphIds, ...
+                "The list of sub-graphs should be empty after a reset.");
+        end
+        
         function checkRootsOfSubGraphWithOnlyOneElement(testCase)
             % Check method "rootsOfSubGraph" when a sub-graph has only one
             % element.
@@ -200,23 +303,6 @@ classdef tDataStructure < amsla.test.tools.AmslaTest
             
             testCase.verifyEqual( ...
                 rootsOfSubGraph(aGraph, 3), ...
-                expectedOutput);
-        end
-        
-        function checkComponentOfNodeVector(testCase)
-            % Check method "componentOfNode" after components have been
-            % computed, when inputs are a vector.
-            
-            % Create a graph
-            [aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
-            
-            aGraph.computeComponents();
-            
-            inputs = 1:10;
-            expectedOutput = [ones(1,8), 2*ones(1,2)];
-            
-            testCase.verifyEqual( ...
-                componentOfNode(aGraph, inputs), ...
                 expectedOutput);
         end
         
@@ -270,6 +356,31 @@ classdef tDataStructure < amsla.test.tools.AmslaTest
                 @() aGraph.setSubGraphOfNode(inputs, subGraphs));
         end
         
+        %% Time-slot API
+        
+        function assignmentOfEdgeToTimeSlotIsRecordedCorrectly(testCase)
+            % Check that the assignment to an edge to a time-slot is recorded
+            % correctly in the object.
+            
+            [aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
+            
+            edgeId = 1;
+            initialTimeSlot = aGraph.timeSlotOfEdge(edgeId);
+            testCase.verifyEqual(initialTimeSlot, iNullId(), ...
+                "Initially, all edges should be assigned to time-slot ID null.");
+            
+            expectedTimeSlotId = 10;
+            aGraph.setTimeSlotOfEdge(edgeId, expectedTimeSlotId);
+            actualTimeSlotId = aGraph.timeSlotOfEdge(edgeId);
+            testCase.verifyEqual(actualTimeSlotId, expectedTimeSlotId, ...
+                "The edge was not correctly assigned to the time-slot.");
+            
+            aGraph.resetTimeSlots();
+            actualTimeSlotId = aGraph.timeSlotOfEdge(edgeId);
+            testCase.verifyEqual(actualTimeSlotId, iNullId(), ...
+                "After a reset, all edges should be assigned to time-slot ID null.");
+        end
+        
     end
 end
 
@@ -279,4 +390,16 @@ function [sortedI, sortedJ] = iSortEdges(I,J)
 sortedEdges = sortrows([I', J'], [1, 2]);
 sortedI = sortedEdges(:, 1);
 sortedJ = sortedEdges(:, 2);
+end
+
+function aGraph = iFullAssignedSimpleGraph()
+[aGraph, ~, ~, ~] = amsla.test.tools.getSimpleLowerTriangularMatrix();
+
+aGraph.setSubGraphOfNode( ...
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ...
+    [1, 1, 1, 2, 2, 3, 4, 4, 5, 5]);
+end
+
+function id = iNullId()
+id = amsla.common.nullId();
 end
